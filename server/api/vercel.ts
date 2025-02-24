865a986ab88db243ada3f5578d73aff75eaad3c2
Oscar Vallecillo <oscarvallecillo95@gmail.com>
2025-02-23 22:23:18 -0600

    deploy

---
+++ server/api/vercel.ts
@@ -0,0 +1,6 @@
+import expressApp from "../server.index";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  return expressApp(req, res);
+}
