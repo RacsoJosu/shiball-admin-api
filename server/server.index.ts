b452f6da7a8c97f80029b049d626f605eb596746
Oscar Vallecillo <oscarvallecillo95@gmail.com>
2025-03-08 23:18:15 -0600

    server vercel

--- server/server.index.ts
+++ server/server.index.ts
@@ -34,7 +34,7 @@ app.use((req: Request, res: Response) => {
 app.use(errorHandler);
 
 prismaConnect();
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 export default app
