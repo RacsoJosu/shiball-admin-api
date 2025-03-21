ccfa39ba9653218d9f6a7821e8130410a189fc6e
Oscar Vallecillo <oscarvallecillo95@gmail.com>
2025-03-20 22:50:01 -0600

    fix: schema registrar user

--- server/api/auth/auth.schemas.ts
+++ server/api/auth/auth.schemas.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 
 export const registerUserSchema = z.object({
     email: z.string().email({message: "El valor ingresado no es un email"}),
-    password: z.string().min(8, { message: "debe tener un mínimo de 8 caracteres"}).max(16, { message: "debe tener un máximo de 16 caracteres"}).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ])$/),
+    password: z.string().min(8, { message: "debe tener un mínimo de 8 caracteres"}).max(16, { message: "debe tener un máximo de 16 caracteres"}),
     firstName: z.string().min(2 ,{ message: "debe tener un mínimo de 2 caracteres"}),
     lastName: z.string().min(2, { message: "debe tener un mínimo de 2 caracteres"}),
     birthDate: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, { message: "el valor debe estar el siguiente formato AAAA-DD-MM"}),
