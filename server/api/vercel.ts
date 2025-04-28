b034e35ac438a96f37d7a8948ce748cbc5012b4b
Oscar Vallecillo <oscarvallecillo95@gmail.com>
2025-04-27 21:38:26 -0600

    path fix

--- server/api/vercel.ts
+++ server/api/vercel.ts
@@ -1,5 +1,5 @@
-import expressApp from "@/server.index";
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import expressApp from '../server.index';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   return expressApp(req, res);
